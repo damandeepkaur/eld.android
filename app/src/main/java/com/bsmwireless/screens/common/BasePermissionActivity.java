395c4f374953932ec0edd694515e76ab839c2caf
Mekhtiev, Maxim <mekhtiev@mera.ru>
2017-06-20 19:46:53 +0300

    EA-28 #comment unit tests for user login presenter

--- app/src/main/java/com/bsmwireless/screens/common/BasePermissionActivity.java
+++ app/src/main/java/com/bsmwireless/screens/common/BasePermissionActivity.java
@@ -2,9 +2,7 @@ package com.bsmwireless.screens.common;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
