bc84abcd0009ed48b7f9f4f974d93de5b2780102
William Chao <william.chao@bsmtechnologies.com>
2017-07-07 09:19:30 -0700

    EA-116: #comment updated API base URL

--- app/src/main/java/com/bsmwireless/common/Constants.java
+++ app/src/main/java/com/bsmwireless/common/Constants.java
@@ -8,6 +8,6 @@ public interface Constants {
     String UI_THREAD = "UI_THREAD";
     String IO_THREAD = "IO_THREAD";
 
-    String BASE_URL = "https://devsd.bsmtechnologies.com/sdmobile/rest/";
+    String BASE_URL = "https://develd.bsmtechnologies.com/sdmobile/rest/";
     String DEVICE_TYPE = "Android";
 }
