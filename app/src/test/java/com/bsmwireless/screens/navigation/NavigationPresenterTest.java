3f8a97783953a4355299c8279605c18dc0b6a91e
William Chao <william.chao@bsmtechnologies.com>
2017-07-26 13:55:06 -0700

    EA-253: #comment added additional test for getUpdatedUser

--- app/src/test/java/com/bsmwireless/screens/navigation/NavigationPresenterTest.java
+++ app/src/test/java/com/bsmwireless/screens/navigation/NavigationPresenterTest.java
@@ -6,6 +6,7 @@ import com.bsmwireless.domain.interactors.VehiclesInteractor;
 import com.bsmwireless.models.ResponseMessage;
 import com.bsmwireless.models.User;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -14,10 +15,15 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import app.bsmuniversal.com.RxSchedulerRule;
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -85,7 +91,7 @@ public class NavigationPresenterTest {
         verify(mView).showErrorMessage(anyString());    // TODO: change to check string resource when no longer hard-coded
 
         verify(mView, never()).goToLoginScreen();   // redundant for now, but want to enforce no
-                                                    // transition in future on failure
+                                                    // future transition to login on failure
     }
 
     @Test
@@ -106,10 +112,13 @@ public class NavigationPresenterTest {
         // given
         final String name = "userName";
         final Flowable<String> userFlowable = Flowable.just(name);
+
         final int coDriver = 1; // note: business logic is incorrect as can have multiple co-drivers
         // TODO: add refactor task/story to JIRA after server-side API refactors to match correct business logic
+
         final int boxId = 1111;
         final int assetNumber = 2222;
+
         when(mLoginUserInteractor.getFullName()).thenReturn(userFlowable);
         when(mLoginUserInteractor.getCoDriversNumber()).thenReturn(coDriver);
         when(mVehiclesInteractor.getBoxId()).thenReturn(boxId);
@@ -147,10 +156,71 @@ public class NavigationPresenterTest {
         verify(mLoginUserInteractor).updateUserOnServer(any(User.class)); // TODO: constrain test further once NavigationPresenter#getUpdatedUser is no longer a stub
     }
 
-    // TODO: add test for LoginUserInteractor#updateUserOnServer failure when control path implemented
+    /**
+     * Test getUpdatedUser.
+     *
+     * TODO: update test when function is redone - current code might be temporary
+     *
+     * Note that the method's logic is testable, although the access is private. Pls. remove test if
+     * this implementation is temporary or not meant to be tested.
+     */
+    @Test
+    public void testGetUpdatedUser() {
+        // given
+        final int id = 123;
+        final String timezone = "UTC+3";
+        final String firstName = "First";
+        final String lastName = "Last";
+        final int cycleCountry = 1;
+        final String signature = "51,377;89,310;89,310;111,281;111,281;130,265;-544,-1";
+        final String address = "123 Test Ave";
+
+        boolean isReflectionSuccess = true;
+        User user = null;
+
+        UserEntity testUser = new UserEntity();
+        testUser.setId(id);
+        testUser.setTimezone(timezone);
+        testUser.setFirstName(firstName);
+        testUser.setLastName(lastName);
+        testUser.setCycleCountry(cycleCountry);
+        testUser.setSignature(signature);
+        testUser.setAddress(address);
+
+        Method getUpdatedUser = null;
+        try {
+            getUpdatedUser = NavigationPresenter.class.getDeclaredMethod("getUpdatedUser", UserEntity.class);
+            getUpdatedUser.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace(); // give clues in CI log
+            isReflectionSuccess = false;
+        }
 
 
-    // TODO: add test for LoginUserInteractor#updateUserOnServer error when user-observable workflow complete
-    // TODO: add tests for getUpdatedUser if/when it becomes testable
+        // when
+        try {
+            Object value = getUpdatedUser.invoke(mNavigationPresenter, testUser);
+            user = (User)value;
+        } catch (IllegalAccessException | InvocationTargetException | NullPointerException e) {
+            e.printStackTrace(); // give clues in CI log
+            isReflectionSuccess = false;
+        }
+
+
+        // then
+        assertTrue(isReflectionSuccess);
+
+        if(user != null) {
+            assertEquals(testUser.getId(), user.getId());
+            assertEquals(testUser.getTimezone(), user.getTimezone());
+            assertEquals(testUser.getFirstName(), user.getFirstName());
+            assertEquals(testUser.getLastName(), user.getLastName());
+            assertEquals(testUser.getCycleCountry(), user.getCycleCountry());
+            assertEquals(testUser.getSignature(), user.getSignature());
+            assertEquals(testUser.getAddress(), user.getAddress());
+        } else {
+            Assert.fail("Error when invoking getUpdatedUser. Please update function or test.");
+        }
+    }
 
 }
