84cc6918c574e5b270c162adf4339cc2e0321648
Anastasia Artemyeva <anartem@mera.ru>
2017-07-27 16:40:48 +0300

    EA-257 #comment Update barcode scanner

--- app/src/main/java/com/bsmwireless/screens/barcode/BarcodeScannerFragment.java
+++ app/src/main/java/com/bsmwireless/screens/barcode/BarcodeScannerFragment.java
@@ -1,33 +1,36 @@
 package com.bsmwireless.screens.barcode;
 
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.bsmwireless.screens.common.BasePermissionFragment;
-import com.google.zxing.Result;
+import com.bsmwireless.screens.common.BaseFragment;
 
 import me.dm7.barcodescanner.core.IViewFinder;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class BarcodeScannerFragment extends BasePermissionFragment implements ZXingScannerView.ResultHandler {
+public class BarcodeScannerFragment extends BaseFragment {
 
     private static final String TAG = BarcodeScannerFragment.class.getSimpleName();
 
-    ZXingScannerView mScannerView;
+    private ZXingScannerView mScannerView;
+    private ZXingScannerView.ResultHandler mHandler;
 
-    public BarcodeScannerFragment() {
-        // Required empty public constructor
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof ZXingScannerView.ResultHandler) {
+            mHandler = (ZXingScannerView.ResultHandler) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must ZXingScannerView.ResultHandler");
+        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mScannerView = new ZXingScannerView(getContext()) {
             @Override
             protected IViewFinder createViewFinderView(Context context) {
@@ -38,41 +41,16 @@ public class BarcodeScannerFragment extends BasePermissionFragment implements ZX
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mScannerView.setResultHandler(this);
+    public void onStart() {
+        super.onStart();
+        mScannerView.setResultHandler(mHandler);
         mScannerView.startCamera();
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
+    public void onStop() {
+        super.onStop();
+        mScannerView.setResultHandler(null);
         mScannerView.stopCamera();
-    }
-
-    @Override
-    public void handleResult(Result result) {
-        Activity activity = getActivity();
-        if (activity instanceof BarcodeResultHandler) {
-            ((BarcodeResultHandler) activity).handleResult(result);
-        }
-    }
-
-    @Override
-    protected String[] getDesiredPermissions() {
-        return new String[] { Manifest.permission.CAMERA };
-    }
-
-    @Override
-    protected void onPermissionDenied() {
-        Activity activity = getActivity();
-        if (activity instanceof BarcodeResultHandler) {
-            ((BarcodeResultHandler) activity).onFinish();
-        }
-    }
-
-    @Override
-    protected void onPermissionGranted() {
-
     }
 }
