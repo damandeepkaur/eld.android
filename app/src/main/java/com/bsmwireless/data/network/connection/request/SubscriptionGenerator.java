7d7e17ba1d0129399900d542e4d165b3e66dbf62
hsudhagar <hemalatha.sudhagar@webtechwireless.com>
2017-08-02 17:09:57 -0700

    EA-209 #comment checkin the request generator

--- app/src/main/java/com/bsmwireless/data/network/connection/request/SubscriptionGenerator.java
+++ app/src/main/java/com/bsmwireless/data/network/connection/request/SubscriptionGenerator.java
@@ -7,6 +7,8 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
+import timber.log.Timber;
+
 /**
  *  Subscription request generator , sent during the beginning of the communication
  */
@@ -42,26 +44,54 @@ public class SubscriptionGenerator extends RequestGenerator {
         //in UTC
         GregorianCalendar cal = new GregorianCalendar();
         cal.setTimeZone(TimeZone.getTimeZone("UTC"));
-        byte[] yrBytes = ConnectionUtils.intToByte(cal.get(Calendar.YEAR),2);
+        int yr = cal.get(Calendar.YEAR);
+        byte[] yrBytes = ConnectionUtils.intToByte(yr,2);
         System.arraycopy(yrBytes, 0, request, index, 2); // 2 bytes
+        StringBuilder sbYr = new StringBuilder(yrBytes.length * 2);
+        for(byte b: yrBytes)
+            sbYr.append(String.format("%02x", b));
+        String stringYr = sbYr.toString();
 
         index += 2;
-        request[index++] = (byte)cal.get(Calendar.MONTH);
+        request[index++] = (byte)(cal.get(Calendar.MONTH)+1);
+        int day = (byte)cal.get(Calendar.DAY_OF_MONTH);
         request[index++] = (byte)cal.get(Calendar.DAY_OF_MONTH);
         request[index++] = (byte)cal.get(Calendar.HOUR_OF_DAY);
+        int h = (byte)cal.get(Calendar.HOUR_OF_DAY);
+        int month = (byte)cal.get(Calendar.MONTH);
         request[index++] = (byte)cal.get(Calendar.MINUTE);
+        int minute = (byte)cal.get(Calendar.MINUTE);
         request[index++] = (byte)cal.get(Calendar.SECOND);
+        int sec = (byte)cal.get(Calendar.SECOND);
+        int MSeconds = (byte)cal.get(Calendar.MILLISECOND);
         byte[] msBytes = ConnectionUtils.intToByte(cal.get(Calendar.MILLISECOND), 2);
         System.arraycopy(msBytes, 0, request, index, 2); // 2 bytes
+        StringBuilder sbms = new StringBuilder(msBytes.length * 2);
+        for(byte b: msBytes)
+            sbms.append(String.format("%02x", b));
+        String stringMS = sbms.toString();
 
         index += 2;                            // 9 bytes for ts
-
+Timber.i("Subscription generator time:" + yr +"/" +month+"/"+day+"/" +minute+"/"+sec+"/"+MSeconds );
         // 1- To recieve Gps data 0- no Gps data
         //Always received BOX GPS Data
         request[index++] = (byte)GPS_DATA;
         //Vehicle Status Update Rate
         request[index++] = (byte) Constants.STATUS_UPDATE_RATE_SECS;
         request[CHECK_SUM_INDX] = ConnectionUtils.checkSum(request, 9);
+        StringBuilder sb = new StringBuilder(request.length * 2);
+        for(byte b: request)
+            sb.append(String.format("%02x", b));
+        String s = sb.toString();
+        Timber.i("Subscription generator hex:" + s );
         return request;
+    }
+
+    public static void main(String[] args)
+    {
+
+        SubscriptionGenerator request = new SubscriptionGenerator();
+        byte[] req= request.generateRequest((byte)2, 74420);
+        Timber.i("check now");
     }
 }
