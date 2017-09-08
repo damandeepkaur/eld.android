20cc738c0724d0400ec4ffe4f616ed83a83bced4
William Chao <william.chao@bsmtechnologies.com>
2017-09-07 17:26:06 -0700

    more tests for BlackBoxInteractor

--- app/src/test/java/com/bsmwireless/domain/interactors/BlackBoxInteractorTest.java
+++ app/src/test/java/com/bsmwireless/domain/interactors/BlackBoxInteractorTest.java
@@ -1,5 +1,6 @@
 package com.bsmwireless.domain.interactors;
 
+import com.bsmwireless.data.network.blackbox.BlackBox;
 import com.bsmwireless.data.network.blackbox.BlackBoxConnectionManager;
 import com.bsmwireless.models.BlackBoxModel;
 
@@ -16,8 +17,11 @@ import app.bsmuniversal.com.RxSchedulerRule;
 import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -32,6 +36,9 @@ public class BlackBoxInteractorTest {
 
     @Mock
     BlackBoxConnectionManager mBlackBoxConnectionManager;
+
+    @Mock
+    BlackBox mBlackBox;
 
 
     private BlackBoxInteractor mBlackBoxInteractor;
@@ -103,6 +110,111 @@ public class BlackBoxInteractorTest {
 
         // then
         testObserver.assertError(boxException);
+    }
+
+    @Test
+    public void testGetLastData() {
+        // given
+        BlackBoxModel blackBoxModel = new BlackBoxModel();
+
+        when(mBlackBoxConnectionManager.getBlackBox()).thenReturn(mBlackBox);
+        when(mBlackBox.getBlackBoxState()).thenReturn(blackBoxModel);
+
+        // when
+        mBlackBoxInteractor.getLastData();
+
+        // then
+        verify(mBlackBoxConnectionManager).getBlackBox();
+        verify(mBlackBox).getBlackBoxState();
+    }
+
+    @Test
+    public void testGetLastDataNullBox() {
+        // given
+        BlackBoxModel blackBoxModel = new BlackBoxModel();
+
+        when(mBlackBoxConnectionManager.getBlackBox()).thenReturn(null);
+        when(mBlackBox.getBlackBoxState()).thenReturn(blackBoxModel);
+
+        // when
+        BlackBoxModel result = mBlackBoxInteractor.getLastData();
+
+        // then
+        verify(mBlackBoxConnectionManager).getBlackBox();
+        verify(mBlackBox, never()).getBlackBoxState();
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testShutdown() {
+        // given
+        String emittedString = "box off";
+        Boolean emittedBoolean = true;
+        Integer emittedInteger = 1231241;
+
+        TestObserver<String> testObserverString = TestObserver.create();
+        TestObserver<Boolean> testObserverBoolean = TestObserver.create();
+        TestObserver<Integer> testObserverInteger = TestObserver.create();
+
+        when(mBlackBoxConnectionManager.disconnectBlackBox())
+                .thenReturn(Observable.just(mBlackBoxConnectionManager));
+
+        // when
+        Observable<String> observableString = mBlackBoxInteractor.shutdown(emittedString);
+        Observable<Boolean> observableBoolean = mBlackBoxInteractor.shutdown(emittedBoolean);
+        Observable<Integer> observableInteger = mBlackBoxInteractor.shutdown(emittedInteger);
+
+        observableString.subscribe(testObserverString);
+        observableBoolean.subscribe(testObserverBoolean);
+        observableInteger.subscribe(testObserverInteger);
+
+        // then
+        testObserverString.assertResult(emittedString);
+        testObserverBoolean.assertResult(emittedBoolean);
+        testObserverInteger.assertResult(emittedInteger);
+    }
+
+    @Test
+    public void testShutdownBoxError() {
+        // given
+        Exception error = new Exception("sorry Dave, I'm afraid I can't do that");
+        when(mBlackBoxConnectionManager.disconnectBlackBox()).thenReturn(Observable.error(error));
+        TestObserver<String> testObserverString = TestObserver.create();
+
+        // when
+        mBlackBoxInteractor.shutdown("any string").subscribe(testObserverString);
+
+        // then
+        testObserverString.assertError(error);
+    }
+
+    @Test
+    public void testGetVinNumber() {
+        // given
+        String expectedVin = "SCAZY19C3WCX80710";
+        when(mBlackBoxConnectionManager.getBlackBox()).thenReturn(mBlackBox);
+        when(mBlackBox.getVinNumber()).thenReturn(expectedVin);
+
+        // when
+        String result = mBlackBoxInteractor.getVinNumber();
+
+        // then
+        assertEquals(expectedVin, result);
+        verify(mBlackBox).getVinNumber();
+    }
+
+    @Test
+    public void testGetVinNumberNullBox() {
+        // given
+        String expectedVin = "";
+        when(mBlackBoxConnectionManager.getBlackBox()).thenReturn(null);
+
+        // when
+        String result = mBlackBoxInteractor.getVinNumber();
+
+        // then
+        assertEquals(expectedVin, result);
+        verify(mBlackBox, never()).getVinNumber();
     }
 
 }
