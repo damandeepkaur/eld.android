2ca4fad9ce2ca57921de4e5a15388cbd4c86a256
ustadenis <denustav@mera.ru>
2017-09-22 14:28:04 +0300

    Default value for co-drivers.

--- app/src/main/java/com/bsmwireless/data/storage/users/UserEntity.java
+++ app/src/main/java/com/bsmwireless/data/storage/users/UserEntity.java
@@ -42,7 +42,7 @@ public final class UserEntity {
     @ColumnInfo(name = "last_vehicle_ids")
     private String mLastVehicleIds;
     @ColumnInfo(name = "co_drivers_ids")
-    private String mCoDriversIds;
+    private String mCoDriversIds = "";
     @ColumnInfo(name = "account_name")
     private String mAccountName;
 
